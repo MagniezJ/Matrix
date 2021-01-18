69bb2aa149d286e2d84f26c6be2e05e8f7640365
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2021-01-18 15:46:09 +0100

    instanciation back 2

---
+++ Back/app/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const UsersRoutes = require('./routes/index'); //routes 
+const bodyParser = require('body-parser');
+  
+
+const cors= require('cors');
+var corsOptions={
+  origin:"http://localhost:3000" //URL front
+}
+app.use(cors(corsOptions))
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use('/',UsersRoutes);
+app.set("json spaces",2)
+
+module.exports = app;
---
+++ Back/model/user-model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt=require('bcrypt');
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    email:String,
+    Password: String,
+    Nom: String,
+    Prenom: String
+},{collection: "USER_COLLEC"})
+
+UserSchema.pre('save',async function(next){
+    try{
+        const Salt=await bcrypt.genSalt(10);
+        const Hash=await bcrypt.hash(this.Password,Salt);
+        this.Password=Hash;
+        next();
+    }catch(error){
+        next(error);
+    };
+}); 
+
+const User = mongoose.model('user',UserSchema);
+
+module.exports = User;
